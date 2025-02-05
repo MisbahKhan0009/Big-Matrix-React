f7492fc4e8739b9f2bc41954e35e7c8442eac015
Misbah Khan <misbah.khan@northsouth.edu>
2025-02-06 00:45:08 +0600

    💄 style fixed

--- src/components/ui/PublicationCard.jsx
+++ src/components/ui/PublicationCard.jsx
@@ -12,7 +12,7 @@ const PublicationCard = ({ publication, onOpenModal }) => {
       <div className="flex justify-between items-start gap-4">
         <div className="flex-1">
           <h3 className="md:text-xl text-base font-medium text-gray-900 mb-1">{publication.paper_title}</h3>
-          <p className="md:text-sm text-xs bg-gray-300 border-l-2 border-l-primary w-fit px-2 text-gray-600 mb-2">{publication.author_name}</p>
+          <p className="md:text-sm text-xs bg-gray-300 border-l-2 border-l-black w-fit px-2 text-gray-600 mb-2">{publication.author_name}</p>
 
           <p className="md:text-sm text-xs text-green-600 bg-green-500/10 p-1 rounded-full w-fit">
             {publication.journal_name}, {publication.year}
@@ -22,7 +22,7 @@ const PublicationCard = ({ publication, onOpenModal }) => {
         <div className="flex items-center gap-2">
           {publication.link && (
             <div className="relative group">
-              <a href={publication.link} target="_blank" rel="noopener noreferrer" className="text-primary hover:text-primary/80">
+              <a href={publication.link} target="_blank" rel="noopener noreferrer" className="text-grey hover:text-grey/80">
                 <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
               </a>
               <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-2 py-1 rounded text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">See Publication</div>
